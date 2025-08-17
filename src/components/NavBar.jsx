067f021955126c7fee264210c200da13629e791b
pankajkoree <pankajkori2057@gmail.com>
2025-08-17 17:35:19 +0545

    navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+const { Button } = require("./ui/button");
+
+const NavBar = () => {
+  return (
+    <div>
+      <Button>Movies</Button>
+      <Button>TV Shows</Button>
+      <Button>Watchlist</Button>
+      <Button>SignIn</Button>
+    </div>
+  );
+};
