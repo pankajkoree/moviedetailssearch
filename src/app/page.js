fdf8089367c07590816c9f8bc2d4b36dba278e1e
pankajkoree <pankajkori2057@gmail.com>
2025-08-19 09:12:35 +0545

    navabar working

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import { NavBar } from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <div>
-      <NavBar />
+    <div className="flex justify-center w-full bg-amber-700">
+      <div className="flex justify-center bg-amber-200 lg:w-[60%]">
+        <NavBar />
+      </div>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,12 @@ const { Button } = require("./ui/button");
 
 export const NavBar = () => {
   return (
-    <div>
-      <Button>Movies</Button>
-      <Button>TV Shows</Button>
-      <Button>Watchlist</Button>
-      <Button>SignIn</Button>
+    <div className="flex gap-2 mt-2 mb-2">
+      <Button variant="movieNavButton">Movies</Button>
+      <Button variant="movieNavButton">TV Shows</Button>
+      <Button variant="movieNavButton">Watchlist</Button>
+      <Button variant="movieNavButton">SignIn</Button>
+      <Button variant="movieNavButtonTheme"></Button>
     </div>
   );
 };
