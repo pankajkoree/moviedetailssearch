02b0ed2c51c3e8c7945fa52f046f8febf8592b7b
pankajkoree <pankajkori2057@gmail.com>
2025-08-21 08:48:05 +0545

    trailer slider done

--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -3,9 +3,17 @@ import { useState } from "react";
 
 export const VideoComponent = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-   const videos = [
-    "https://www.youtube.com/watch?v=PJLAoYYocsI",
-    "https://www.youtube.com/watch?v=ClWFuntWmy0",
+  const videos = [
+    "fsQgc9pCyDU",
+    "dK1W-AViQ-M",
+    "5lY5NJgJMBs",
+    "X38mG7IW4mo",
+    "CnEOLuCojY0",
+    "r-7g08INMSI",
+    "y35F2nMfVwY",
+    "YShVEXb7-ic",
+    "8SKJvLEnwkI",
+    "tA1s65o_kYM",
   ];
 
   const handleNext = () => {
@@ -16,30 +24,56 @@ export const VideoComponent = () => {
     setCurrentIndex((prev) => (prev - 1 + videos.length) % videos.length);
   };
   return (
-    <div className="relative flex p-4">
-      <video
-        key={videos[currentIndex]}
-        className="rounded-xl shadow-lg"
-        width="600"
-        controls
-      >
-        <source src={videos[currentIndex]} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-
-      <div className="flex gap-6">
-        <button
+    <div className="relative flex p-4 gap-4">
+      <div className="flex z-10 items-center mr-[-60px] dark:text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          width="40"
+          height="60"
           onClick={handlePrev}
-          className="px-4 py-2 bg-gray-200 rounded-lg shadow hover:bg-gray-300"
+          className="border p-2 hover:cursor-pointer bg-slate-400"
         >
-          ⬅️ Prev
-        </button>
-        <button
+          <path
+            d="M15 6 L9 12 L15 18"
+            stroke="currentColor"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
+      <div>
+        <iframe
+          key={videos[currentIndex]}
+          width="840"
+          height="516"
+          src={`https://www.youtube.com/embed/${videos[currentIndex]}`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="flex items-center ml-[-60px] dark:text-white">
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="60"
           onClick={handleNext}
-          className="px-4 py-2 bg-gray-200 rounded-lg shadow hover:bg-gray-300"
+          className="border p-2 hover:cursor-pointer bg-slate-400"
         >
-          Next ➡️
-        </button>
+          <path
+            d="M9 6 L15 12 L9 18"
+            stroke="currentColor"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </div>
     </div>
   );
