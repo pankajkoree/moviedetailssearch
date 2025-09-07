7034469cba3c1ce0c41b174e64c3d558f6d8cf46
pankajkoree <pankajkori2057@gmail.com>
2025-09-07 08:08:21 +0545

    halt

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -215,6 +215,7 @@ const Movies = () => {
                     </div>
                     {/* overview */}
                   </div>
+                  {/* idk what doing, but stopping this fr */}
 
                   {/* 3rd section for info and details */}
                 </div>
